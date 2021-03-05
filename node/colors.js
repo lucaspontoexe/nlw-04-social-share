df4209c342bbd9b131ec94425cefbd0aedcef407
lucaspontoexe <ltx16@hotmail.com>
2021-03-05 18:59:29 -0300

    add resources

---
+++ node/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    blue1: "#5965E0",
+    blue2: "#4953B8",
+    green: "#4CD62B",
+    red: "#E83F5B",
+    blueText: "#B2B9FF",
+    titles: "#2E384D",
+    texts: "#666666",
+    grayLine: "#DCDDE0",
+    background: "#F2F3F5",
+  };
+
+  module.exports = colors;
--- node/index.js
+++ node/index.js
@@ -1,7 +1,9 @@
 // https://nodejs.org/en/knowledge/HTTP/clients/how-to-access-query-string-parameters/
 const http = require('http');
 const url = require('url');
-const canvas = require('canvas');
+const {loadImage, registerFont, createCanvas} = require('canvas');
+const Box = require('./Box');
+const colors = require('./colors');
 
 http.createServer(async function (req, res) {
   const queryObject = url.parse(req.url,true).query;
@@ -12,3 +14,12 @@ http.createServer(async function (req, res) {
   res.end('Feel free to add query parameters to the end of the url');
 }).listen(8080);
 
+
+async function drawCanvas() {
+    registerFont('./Inter.ttf', {family: 'Inter'});
+    const moveitLogo = await loadImage('./Logo.svg');
+    const leafL = await loadImage('./leaf-L.svg');
+    const leafR = await loadImage('./leaf-R.svg');
+
+    
+}
